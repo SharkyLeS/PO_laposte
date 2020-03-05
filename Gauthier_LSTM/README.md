48b6b26d1ee6d6318cf4a4fa5e01f35c4e70e50b
Gauthier <gauth.gris@gmail.com>
2020-03-05 11:40:38 +0100

    grouping data

--- Gauthier_LSTM/README.md
+++ Gauthier_LSTM/README.md
@@ -1,5 +1,7 @@
 # LSTM - Gauthier
 
+Ce dossier comprant le travail réalisé pour résoudre le problème avec une méthode LSTM (réseau de neurones). Le code est écrit en python et une nécessite une version supérieur à 3.0 ([install python3](https://www.python.org/downloads/)).
+
 ## Installation des packages
 
 TODO
@@ -23,5 +25,17 @@ Exemple d'utilisation :
 
 `python dir_path out_path`
 
+### Script *data_grouping.py* 
 
+Ce script permet de grouper les données nettoyées pour un pas de temps donné. Les données sont ansi regroupées sur des intervalles de temps régulier, ceci permet d'avoir des tailles de données d'entrée fixes pour notre réseau de neuronnes.
+
+Inputs : 
+
+1. *data_path* : chemin vers les données nettoyées
+2. *step_grou* : Intervall de temps sur lequel regrouper les données en min
+3. *out_path* : chemin de sauvegarde des données groupées
+
+Exemple d'utilisation :
+
+`python data_path "5min" out_path`
 
---
+++ Gauthier_LSTM/data_grouping.py
@@ -0,0 +1,63 @@
+'''
+Author : Gauthier Gris
+Date : 14 Jan 2020
+'''
+
+# Import des librairies
+import pandas as pd
+import sys
+
+# Paramètres
+data_path = sys.argv[1]  # Chemin vers les données nettoyées
+step_group = sys.argv[2]  # Intervall de temps sur lequel regrouper les données en min
+out_path = sys.argv[3]  # Chemin de sauvegarde des données groupées
+
+# Chargement des données nettoyées dans un DataFrame
+dataFrame = pd.read_csv(data_path)
+
+dataFrame['date'] = dataFrame['date'].apply(lambda x : pd.Timestamp(x))
+dataFrame['CBN'] = pd.Categorical(dataFrame['CBN'])
+
+# Créer les one hot encodings pour chaque variabble
+dfDummies_cbn = pd.get_dummies(dataFrame['CBN'], prefix = 'CBN')
+
+# Concatener les one hot encodings avec les données
+dataFrame = pd.concat([dataFrame, dfDummies_cbn], axis=1)
+
+# Suppression de la variable CBBN
+dataFrame.drop(columns=['CBN'], inplace=True)
+
+# On ordonne les dataFrame par "date"
+dataFrame.sort_values(by='date', inplace=True)
+
+# Réindexage des lignes
+dataFrame.reset_index(inplace=True)
+dataFrame.drop(columns=['index'], inplace=True)
+
+
+grouped_dataFrame = dataFrame.groupby(pd.Grouper(key="date", freq=step_group)).sum()
+grouped_dataFrame.reset_index(inplace=True)
+
+# Ajout variables
+grouped_dataFrame['month'] = grouped_dataFrame['date'].apply(lambda x : x.month)
+grouped_dataFrame['weekday'] = grouped_dataFrame['date'].apply(lambda x : x.weekday())
+grouped_dataFrame['hour'] = grouped_dataFrame['date'].apply(lambda x : x.hour)
+
+
+grouped_dataFrame['month'] = pd.Categorical(grouped_dataFrame['month'])
+grouped_dataFrame['weekday'] = pd.Categorical(grouped_dataFrame['weekday'])
+grouped_dataFrame['hour'] = pd.Categorical(grouped_dataFrame['hour'])
+
+# Créer les one hot encodings pour chaque variabble
+dfDummies_month = pd.get_dummies(grouped_dataFrame['month'], prefix = 'month')
+dfDummies_weekday = pd.get_dummies(grouped_dataFrame['weekday'], prefix = 'weekday')
+dfDummies_hour = pd.get_dummies(grouped_dataFrame['hour'], prefix = 'hour')
+
+# Concatener les one hot encodings avec les données
+grouped_dataFrame = pd.concat([grouped_dataFrame, dfDummies_weekday, dfDummies_month, dfDummies_hour], axis=1)
+
+# Suppression de la variable mois et jour de la semaine
+grouped_dataFrame.drop(columns=['month', 'weekday', 'hour'], inplace=True)
+
+# Sauvegarde des données
+grouped_dataFrame.to_csv(out_path, index=False)
